46fc4cd654b39a3f1ec58daabb444ed01bb3d038
unknown <moosakpmunavir@gmail.com>
2024-11-08 02:11:36 +0530

    Initial commit with full project code

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/footer";
+import { Toaster } from "react-hot-toast";
+import useUserRoutes from "./components/routes/userRoutes";
+import useAdminRoutes from "./components/routes/adminRoutes";
+
+function App() {
+  const userRoutes = useUserRoutes();
+  const adminRoutes = useAdminRoutes();
+  return (
+    <Router>
+      <div className="App">
+        <Toaster position="top-center" />
+        <Header />
+
+        <div className="container">
+          <Routes>
+            {userRoutes}
+            {adminRoutes}
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
